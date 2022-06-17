d41b6b133682a8b4a8bc1628783e7e7c85796a86
José Antonio Peño <74198708+josepebel@users.noreply.github.com>
2022-06-17 20:19:28 +0200

    Update postgresScript.sh

--- scripts/postgresScript.sh
+++ scripts/postgresScript.sh
@@ -2,4 +2,4 @@
 psql 'postgresql://postgres:postgres@localhost:5432/postgres?sslmode=prefer' -c 'CREATE TABLE COMPANY(
    ID INT PRIMARY KEY     NOT NULL,
    NAME           TEXT    NOT NULL,
-   AGE            INT     NOT NULL,'
+   AGE            INT     NOT NULL)'

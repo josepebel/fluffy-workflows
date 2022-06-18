c53e7c32716db03d9e9b0f41fafc9f12f0b0c5d5
José Antonio Peño <74198708+josepebel@users.noreply.github.com>
2022-06-19 00:39:03 +0200

    Update people.sql

--- scripts/people.sql
+++ scripts/people.sql
@@ -5,7 +5,7 @@ CREATE TABLE original_public.people(
    NAME           TEXT    NOT NULL,
    AGE            INT     NOT NULL);
    
-INSERT INTO original_public.people (id, name, age)
+INSERT INTO original_public.people (id, name, age, active)
 VALUES 
-(1, 'Jose', 29), 
-(2, 'Edu', 23);
+(1, 'Jose', 29, true), 
+(2, 'Edu', 23, true);

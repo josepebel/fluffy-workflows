4d02da3c5a714da14266ae867532a6eff82bd13d
José Antonio Peño <74198708+josepebel@users.noreply.github.com>
2022-06-19 00:08:00 +0200

    Create people.sql

---
+++ scripts/people.sql
@@ -0,0 +1,9 @@
+CREATE TABLE PEOPLE(
+   ID INT PRIMARY KEY     NOT NULL,
+   NAME           TEXT    NOT NULL,
+   AGE            INT     NOT NULL);
+   
+INSERT INTO people (id, name, age)
+VALUES 
+(1, 'Jose', 29), 
+(2, 'Edu', 23);
